7b7dfe606d3ca5dc21353459c15b375c7922e17d
mashimokiyoshi <mashimo.kiyoshi@no1s.biz>
2018-10-04 00:57:15 +0900

    Revert "Revert "adminLTE適用""
    This reverts commit 0affecb561cd346f98b891bfa114b05c4c4e02db.

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
@@ -159,6 +159,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Download Service Providers...
+         */
+
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
 
     ],
 
